58656dc4895cfef43980fe559c1afc0b615b0b6a
Cyril Suzat <cyril.suzat@me.com>
2015-02-06 11:54:33 +0100

    [seeds] Génération d'emails uniques et safe (@example.com...)

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :member do
-    last_name     { Faker::Name.last_name }
-    first_name    { Faker::Name.first_name }
-    registered_on { Faker::Date.backward(15) }
-    email         { Faker::Internet.free_email }
-    street        { Faker::Address.street_address }
-    postal_code   { Faker::Address.postcode }
-    city          { Faker::Address.city }
-    country       { [Faker::Address.default_country, Faker::Address.country].sample }
-    website       { [Faker::Internet.url, nil].sample }
-    phone_number  { [Faker::PhoneNumber.phone_number, nil].sample }
-    mobile_number { [Faker::PhoneNumber.phone_number, nil].sample }    
+    last_name        { Faker::Name.last_name }
+    first_name       { Faker::Name.first_name }
+    registered_on    { Faker::Date.backward(15) }
+    sequence(:email) { |n| Faker::Internet.safe_email("#{first_name}#{n}") }
+    street           { Faker::Address.street_address }
+    postal_code      { Faker::Address.postcode }
+    city             { Faker::Address.city }
+    country          { [Faker::Address.default_country, Faker::Address.country].sample }
+    website          { [Faker::Internet.url, nil].sample }
+    phone_number     { [Faker::PhoneNumber.phone_number, nil].sample }
+    mobile_number    { [Faker::PhoneNumber.phone_number, nil].sample }    
   end
 end
