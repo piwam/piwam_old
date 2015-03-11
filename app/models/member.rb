49cbdde1cbc871cbdecb72dbc0418267d9e98e79
Cyril Suzat <cyril.suzat@me.com>
2015-03-11 09:31:56 +0100

    Ne géolocalise pas si la ville n'est pas renseignée

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,7 +11,7 @@ class Member < ActiveRecord::Base
   belongs_to :updater, class_name: 'Member', foreign_key: 'updated_by'
 
   geocoded_by      :address
-  after_validation :geocode, unless: -> { Rails.env.test? }
+  after_validation :geocode, unless: -> { city.blank? or Rails.env.test? }
 
   has_attached_file :photo,
                     styles: { original: '116x116#' },
